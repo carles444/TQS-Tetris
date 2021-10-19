fa1c1d47d2484836e51af5e651b1ffafef81d8f1
carles444 <carlesandreu4@gmail.com>
2021-10-19 21:06:22 +0200

    implementation rotation blueRicky piece

--- src/main/java/TetrisGame/Piece.java
+++ src/main/java/TetrisGame/Piece.java
@@ -22,11 +22,11 @@ public abstract class Piece {
     }
 
     public int[][] rotateRight() {
-
+        return null;
     }
 
     public int[][] rotateLeft() {
-
+       return null;
     }
 }
 
@@ -42,7 +42,82 @@ class BlueRicky extends Piece {
         positions[2] = new int[]{initPos[0]+1, initPos[1]+1};
         positions[3] = new int[]{initPos[0]+1, initPos[1]+2};
     }
+    @Override
+    public int[][] rotateRight() {
+        if(positions[0][1] == positions[1][1]) {
+            if(positions[0][0] < positions[1][0]) {
+                positions[0][1] += 2;
+                positions[1][0] -= 1;
+                positions[1][1] += 1;
+                positions[3][0] += 1;
+                positions[3][1] -= 1;
+            }
+            else {
+                positions[0][1] -= 2;
+                positions[1][0] += 1;
+                positions[1][1] -= 1;
+                positions[3][0] -= 1;
+                positions[3][1] += 1;
+            }
+        }
+        else {
+            if(positions[0][1] < positions[1][1]) {
+                positions[0][0] -= 2;
+                positions[1][0] -= 1;
+                positions[1][1] -= 1;
+                positions[3][0] += 1;
+                positions[3][1] += 1;
+            }
+            else {
+                positions[0][0] += 2;
+                positions[1][0] += 1;
+                positions[1][1] += 1;
+                positions[3][0] -= 1;
+                positions[3][1] -= 1;
+            }
+        }
+        return positions;
+    }
+
+
+    @Override
+    public int[][] rotateLeft() {
+        if(positions[0][1] == positions[1][1]) {
+            if(positions[0][0] < positions[1][0]) {
+                positions[0][0] += 2;
+                positions[1][0] += 1;
+                positions[1][1] += 1;
+                positions[3][0] -= 1;
+                positions[3][1] -= 1;
+            }
+            else {
+                positions[0][0] -= 2;
+                positions[1][0] -= 1;
+                positions[1][1] -= 1;
+                positions[3][0] += 1;
+                positions[3][1] += 1;
+            }
+        }
+        else {
+            if(positions[0][1] < positions[1][1]) {
+                positions[0][1] += 2;
+                positions[1][0] -= 1;
+                positions[1][1] += 1;
+                positions[3][0] += 1;
+                positions[3][1] -= 1;
+            }
+            else {
+                positions[0][1] -= 2;
+                positions[1][0] += 1;
+                positions[1][1] -= 1;
+                positions[3][0] -= 1;
+                positions[3][1] += 1;
+            }
+        }
+        return positions;
+    }
 }
+
 
 class ClevelandZ extends  Piece {
 
