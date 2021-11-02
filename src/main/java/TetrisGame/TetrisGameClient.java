684951f9f0f2a69f4931bcd51d0732af0c18b675
Charly251097 <38043649+Charly251097@users.noreply.github.com>
2021-11-02 14:04:16 +0100

    TetrisGameClient
    Implementation method create user

--- src/main/java/TetrisGame/TetrisGameClient.java
+++ src/main/java/TetrisGame/TetrisGameClient.java
@@ -1,11 +1,57 @@
 package TetrisGame;
 
 
+import java.util.ArrayList;
 
 public class TetrisGameClient {
     TetrisGameClient(){}
-    public boolean createUser(String name, String pasword){
-return true;
-    }
 
+    public boolean createUser(String name, String pasword){
+        return isPasswordFormatCorrect(pasword)&&isUserNameFormatCorrect(name);
+    }
+    //region---------------------------------Password Format------------------------------------------
+
+    private boolean isPasswordFormatCorrect(String password){
+        boolean haveUpperCase=false;
+        boolean haveLowerCase=false;
+        boolean haveDigit=false;
+        boolean haveSpecialCaracter=false;
+        if ((isEmptyOrCantainScape(password) ||isIncorrectLenght(password) )) {
+            return false;
+        }
+        for (int x = 0; x < password.length(); x++) {
+            char c = password.charAt(x);
+            haveUpperCase= (haveUpperCase || isUpperCase(c))?true:false;
+            haveLowerCase= (haveLowerCase || isLowerCase(c))?true:false;
+            haveDigit= (haveDigit || isDigit(c))?true:false;
+            haveSpecialCaracter= (haveSpecialCaracter || isSpecialCaracter(c))?true:false;
+        }
+        return haveUpperCase && haveLowerCase && haveDigit && haveSpecialCaracter;
+    }
+    private boolean isEmptyOrCantainScape(String password){
+        return password.isEmpty() && !password.contains(" ");
+    }
+    private  boolean isIncorrectLenght(String password){
+        return password.length()!=8;
+    }
+    private boolean isUpperCase(char ch){
+        return Character.isUpperCase(ch);
+    }
+    private boolean isLowerCase(char ch){
+        return Character.isLowerCase(ch);
+    }
+    private boolean isDigit(char ch){
+        return Character.isDigit(ch);
+    }
+    private boolean isSpecialCaracter(char ch){
+        return !Character.isDigit(ch) && !Character.isLetter(ch) && !Character.isWhitespace(ch);
+    }
+    //endregion
+private boolean isUserNameFormatCorrect(String userName){
+
+        if ((isEmptyOrCantainScape(userName) || userName.length()>8)) {
+            return false;
+        }
+        return userName.matches("^[a-zA-Z]*$");
+    }
 }
--- src/test/java/TetrisGame/TetrisGameClientTests.java
+++ src/test/java/TetrisGame/TetrisGameClientTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,32 +22,32 @@ public class TetrisGameClientTests {
     @Test
     void login(){
         //el pasword correcte usuari erroni
-        assertFalse(tetrisGame.createUser("loco","1eG$5dg@"));
-        assertFalse( tetrisGame.createUser("CharlymikelRichartOssart","1eG$5dg@"));
-        assertFalse( tetrisGame.createUser(" ","1eG$5dg@"));
-        assertFalse(tetrisGame.createUser("","1eG$5dg@"));
-        assertFalse( tetrisGame.createUser("Char Y","1eG$5dg@"));
-        assertFalse(tetrisGame.createUser("12345","1eG$5dg@"));
-        assertFalse(tetrisGame.createUser("·$&&","1eG$5dg@"));
-        assertFalse( tetrisGame.createUser("Charly123123","1eG$5dg@"));
-        assertFalse( tetrisGame.createUser("Charly.$&&","1eG$5dg@"));
-        assertFalse( tetrisGame.createUser("Charly123123.%&&","1eG$5dg@"));
+        assertEquals(false, tetrisGame.createUser("CharlymikelRichartOssart","1eG$5dg@"));
+        assertEquals(false, tetrisGame.createUser(" ","1eG$5dg@"));
+        assertEquals(false,tetrisGame.createUser("","1eG$5dg@"));
+        assertEquals(false, tetrisGame.createUser("Char Y","1eG$5dg@"));
+        assertEquals(false,tetrisGame.createUser("12345","1eG$5dg@"));
+        assertEquals(false,tetrisGame.createUser("·$&&","1eG$5dg@"));
+        assertEquals(false, tetrisGame.createUser("Charly123123","1eG$5dg@"));
+        assertEquals(false, tetrisGame.createUser("Charly.$&&","1eG$5dg@"));
+        assertEquals(false, tetrisGame.createUser("Charly123123.%&&","1eG$5dg@"));
         //el pasword correcte usuari erroni
-        assertFalse( tetrisGame.createUser("Charly","a"));
-        assertFalse( tetrisGame.createUser("Charly",""));
+        assertEquals(false, tetrisGame.createUser("Charly","a"));
+        assertEquals(false, tetrisGame.createUser("Charly",""));
 
-        assertFalse( tetrisGame.createUser("Charly"," "));
-        assertFalse( tetrisGame.createUser("Charly","aaaaaaaaa"));
+        assertEquals(false,tetrisGame.createUser("Charly"," "));
+        assertEquals(false, tetrisGame.createUser("Charly","aaaaaaaaa"));
 
-        assertFalse( tetrisGame.createUser("Charly","acvd1265"));
+        assertEquals(false, tetrisGame.createUser("Charly","acvd1265"));
 
-        assertFalse( tetrisGame.createUser("Charly","ahsjdk#~€¬"));
-        assertFalse( tetrisGame.createUser("Charly","1659@€¬$"));
+        assertEquals(false, tetrisGame.createUser("Charly","ahsjdk#~€¬"));
+        assertEquals(false, tetrisGame.createUser("Charly","1659@€¬$"));
 
-        assertFalse( tetrisGame.createUser("Charly","akjfafngjnakjfgnkdjf"));
-        assertFalse( tetrisGame.createUser("Charly","a"));
+        assertEquals(false, tetrisGame.createUser("Charly","akjfafngjnakjfgnkdjf"));
+        assertEquals(false, tetrisGame.createUser("Charly","a"));
         //Correcto
-        assertTrue( tetrisGame.createUser("Charly","1eG$5dg@"));
+        assertEquals(true, tetrisGame.createUser("Charly","1eG$5dg@"));
+        assertEquals(true,tetrisGame.createUser("loco","1eG$5dg@"));
 
 
 
