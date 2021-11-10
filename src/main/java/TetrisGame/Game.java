f4310ea8c8347c700a70d546853c7e15b9ca70ed
carles444 <carlesandreu4@gmail.com>
2021-11-10 20:34:54 +0100

    game constructor implementation

--- src/main/java/TetrisGame/Game.java
+++ src/main/java/TetrisGame/Game.java
@@ -1,12 +1,37 @@
 package TetrisGame;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class Game {
     private Board board;
     private Timer timer;
     private int score;
+    private boolean isRuning;
+    private static long timeInterval = 500;
+    public static int nRows = 20;
+    public static int nCols = 10;
 
+    public Game() {
+        board = new Board(nRows, nCols);
+        score = 0;
+        isRuning = true;
+        timer = new Timer(true);
+        TimerTask tick = new TimerTask() {
+            @Override
+            public void run() {
+                board.movePieceDown();
+            }
+        };
+        timer.scheduleAtFixedRate(tick, 0, timeInterval);
+    }
 
+    public void gameLoop() {
+
+    }
+
+    public int getScore() { return score; }
+    public Board getBoard() { return board; }
+    public boolean isRunning() { return isRuning; }
 
 }
